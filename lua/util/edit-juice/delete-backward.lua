dc8e7cb1f74676a53c366dbad6cedff71752f237
Landon Bouma <landonb@users.noreply.github.com>
2025-03-23 10:51:02 -0500

    fix(delete-backward): adjust nuanced delete-backward-word behavior

--- lua/util/edit-juice/delete-backward.lua
+++ lua/util/edit-juice/delete-backward.lua
@@ -14,97 +14,179 @@ local M = {}
 -- - HSTRY/2020-05-13: I've been using `db` the past 10 years, but
 --   let's behave more elegantly when the cursor is at the beginning
 --   or the end of the line.
+--
+-- - Examples: delete_back_word() behaves almost identical to <Ctrl-W>,
+--   except when used after two or more whitespaces, deletes only the
+--   whitespace, and not the word before.
+--
+--   Consider the following examples, where "|" is the cursor before
+--   and after using <Ctrl-W> compared to delete_back_word().
+--
+--   Text and cursor|     Built-in <C-W>      delete_back_word()
+--   ────────────────     ──────────────      ──────────────────
+--
+--   foo-bar...,|         foo-bar|            foo-bar|
+--   foo        |         |                   foo|
+--   foo |                |                   |
+--   foo", "bar|          foo", "|            foo", "|
+--   foo", "|bar          foo", |bar          foo", |bar
+--   foo", |bar           foo|bar             foo|bar
+--   foo("/", |           foo|                foo|
+--   \n|  --              |  --               |  --                See |kdd|
+--
+-- - Also note the special cases — cursor on empty line, cursor
+--   at line's end, previous line is blank, etc. Because quirks.
 
 function M.delete_back_word()
   -- Mimic `db`, but behave different at end of line, and at beginning.
   local mode = vim.api.nvim_get_mode().mode:sub(1, 1)
   local curr_col = vim.fn.col(".")
-  local line_nbytes = vim.fn.getline("."):len()
+  local curr_line = vim.fn.getline(".")
+  local line_nbytes = curr_line:len()
   if curr_col == 1 then
-    local was_ww = vim.o.whichwrap
-    vim.o.whichwrap = "h"
-    vim.cmd([[normal! dh]])
-    if mode == "i" and line_nbytes == 0 then
-      -- A `dh` on an empty line deletes the newline but moves the cursor one
-      -- before the final character (probably because of virtualedit behavior),
-      -- so jump to the final cursor position.
-      -- DUNNO: Unsure why, but need to both `l` and to call nvim_win_set_cursor...
-      vim.cmd([[normal l]])
-      local cur_win = 0
-      vim.api.nvim_win_set_cursor(cur_win, { vim.fn.line("."), vim.fn.col(".") + 1 })
+    -- If previous line is blank, and if current line starts with
+    -- whitespace, either |dh| or |dN| (using @/ below) deletes
+    -- newline but then moves cursor *after* the whitespace (to
+    -- either the first non-whitespace character, or to EOL).
+    local linenr = vim.fn.line(".")
+    if linenr > 1 and vim.fn.getline(linenr - 1) == "" then
+      -- Ensure cursor stays in the first column.
+      vim.cmd([[normal! kdd]])
+    else
+      local was_ww = vim.o.whichwrap
+      vim.opt.whichwrap:append("h")
+      vim.cmd([[normal! dh]])
+      if mode == "i" and line_nbytes == 0 then
+        -- A `dh` on an empty line deletes the newline but moves the
+        -- cursor one before the final character (probably because of
+        -- virtualedit behavior), so jump to the final cursor position.
+        M.move_insert_cursor_to_end_of_line()
+      end
+      -- E.g., "<,>,[,],b,s"
+      vim.o.whichwrap = was_ww
     end
-    -- E.g., "<,>,[,],b,s"
-    vim.o.whichwrap = was_ww
   else
     local curpos = vim.fn.getcurpos()
     local i_curswant = 5 -- 1-based Lua arrays
     local curswant = curpos[i_curswant]
     local last_col = vim.fn.virtcol("$")
 
-    local last_pttrn = vim.fn.getreg("/")
-    -- vim.o.hlsearch is true|false and vim.v.hlsearch is 1|0.
-    --  local hlsearch = vim.o.hlsearch and vim.v.hlsearch
-    vim.fn.setreg("/", "\\(\\(\\_^\\|\\<\\|\\s\\+\\)\\zs\\|\\>\\)")
-    vim.cmd([[normal! dN]])
-    -- stylua: ignore
-    if (
-      (mode == 'i' and curswant >= last_col)
-      or (mode == 'n' and (curswant + 1) >= last_col)
-    ) then
-      if mode == 'n' then
-        -- The final character escaped the dN motion. Delete it.
-        vim.cmd([[normal! x]])
+    if
+      ((mode == "i" and curswant >= last_col) or (mode == "n" and (curswant + 1) >= last_col))
+      -- E.g., " x$", "foo's$", and "foo-$", respectively:
+      and (
+        curr_line:find("%s%S$")
+        or (vim.fn.match(curr_line, "\\<.$") ~= -1)
+        or (vim.fn.match(curr_line, "\\>.$") ~= -1)
+      )
+    then
+      vim.cmd([[normal! x]])
+      if mode == "i" then
+        M.move_insert_cursor_to_end_of_line()
       end
-      -- Weird: I'm seeing getcurpos() report incorrect curswant.
-      -- E.g., if the line is
-      --         foo bar
-      -- and I C-BS to delete the 'bar' in insert mode, so what's
-      -- left is 'foo ', and the cursor is after the space, the
-      -- curswant should be 5, but getcurpos says 4. If I hit
-      -- '$' though, the cursor does not move, but curswant updates
-      -- to 5. So ensure curswant is accurate if this function
-      -- called again.
-      vim.cmd([[normal! $]])
-      -- Dunno: I ported this code from Vimscript, and now call via
-      -- vim.keymap.set(..., function() ... end), and from Insert mode,
-      -- when you delete from the last column, the previous call,
-      -- `vim.cmd([[normal! $]])`, doesn't move the cursor, which
-      -- remains one column before the last column. Fortunately,
-      -- nvim_win_set_cursor works.
-      -- - Though also need this `l`, what's going on?
-      if mode == 'i' then
-        vim.cmd([[normal l]])
-        local cur_win = 0
-        vim.api.nvim_win_set_cursor(cur_win, {vim.fn.line("."), vim.fn.col('.') + 1})
+    else
+      local last_pttrn = vim.fn.getreg("/")
+
+      -- stylua: ignore
+      vim.fn.setreg("/", ""
+        .. "\\("
+        -- Delete only whitespace if two or more whitespace precede cursor (\%#),
+        -- and stop at WORD boundaries (\\S\\s), not just word boundaries (\\>\\s).
+        -- - So if only one whitespace, this doesn't match, and delete-
+        --   backward deletes the one whitespace and item before it.
+        .. "\\S\\zs\\s\\s\\+\\%#"
+        -- Delete to start of line without gobbling newline.
+        .. "\\|\\_^\\zs"
+        -- Delete to start of word boundary.
+        .. "\\|\\<\\zs"
+        -- Don't stop at word boundary if only one whitespace between word and cursor.
+        -- - This stops at word boundary:
+        --     .. "\\|\\>\\zs"
+        --   - But we want delete backward "foo |" to "|" and not just "foo|".
+        .. "\\|\\>\\zs\\(\\S\\|\\s\\s\\+\\)"
+        -- If single char. WORD followed by whitespace then cursor, delete
+        -- only the whitespace (and don't include the single char. WORD).
+        .. "\\|\\s\\+\\zs"
+        .. "\\)"
+      )
+      -- Here's the same on one line, for easy copy-paste, or ::<CR>.
+      --  let @/ = "\\(\\S\\zs\\s\\s\\+\\%#\\|\\_^\\zs\\|\\<\\zs\\|\\>\\zs\\|\\s\\+\\zs\\)"
+      --
+      -- Or as individually-testable components
+      --  let @/ = "\\S\\zs\\s\\s\\+\\%#"
+      --  let @/ = "\\_^\\zs"
+      --  let @/ = "\\<\\zs"
+      --  let @/ = "\\>\\zs"
+      --  let @/ = "\\s\\+\\zs"
+      --
+      vim.cmd([[normal! dN]])
+
+      -- stylua: ignore
+      if (
+        (mode == 'i' and curswant >= last_col)
+        or (mode == 'n' and (curswant + 1) >= last_col)
+      ) then
+        if mode == 'n' then
+          -- The final character escaped the dN motion. Delete it.
+          vim.cmd([[normal! x]])
+        end
+        -- Weird: I'm seeing getcurpos() report incorrect curswant.
+        -- - E.g., if the line is
+        --     foo bar
+        --   and I C-BS to delete the 'bar' in insert mode, so what's
+        --   left is 'foo ', and the cursor is after the space, the
+        --   curswant should be 5, but getcurpos says 4. If I hit
+        --   '$' though, the cursor does not move, but curswant
+        --   updates to 5. So ensure curswant is accurate if this
+        --   function called again.
+        vim.cmd([[normal! $]])
+        -- Dunno: I ported this code from Vimscript, and now call via
+        -- vim.keymap.set(..., function() ... end), and from Insert mode,
+        -- when you delete from the last column, the previous call,
+        -- `vim.cmd([[normal! $]])`, doesn't move the cursor, which
+        -- remains one column before the last column. Fortunately,
+        -- nvim_win_set_cursor works.
+        if mode == 'i' then
+          M.move_insert_cursor_to_end_of_line()
+        end
       end
+      vim.fn.setreg("/", last_pttrn)
+      -- With Noice, even though we restore the search register (and
+      -- the user sees the same search matches highlighted), the
+      -- current line shows our setreg pattern from above as virtual
+      -- text at the end of the line, e.g.,
+      --         ...     ?\(\(\_^\|\<|\s\+\)\zs\|\>\)   [>99/>99]
+      -- Clearing the search highlight doesn't hide the virtual text:
+      --   vim.cmd.nohlsearch()
+      -- Nor does enabling search highlights for the restored "/" register:
+      --   if hlsearch == 1 then
+      --     vim.o.hlsearch = true
+      --   end
+      -- Nor does mimicking LazyVim <Esc> binding ("Escape and Clear hlsearch"):
+      --   vim.cmd("noh")
+      --   LazyVim.cmp.actions.snippet_stop()
+      -- KLUGE: But for some reason, emitting a space works (and thankfully
+      -- doesn't ellicit noice notification popup).
+      -- SPIKE: What's the api for clearning virtual text? Use that instead?
+      print(" ")
+      vim.cmd("nohlsearch")
     end
-    vim.fn.setreg("/", last_pttrn)
-    -- In LazyVim (I'm not sure which plugin does this), even though
-    -- we restore the search register (and the user sees the same
-    -- search matches highlighted), the current line shows our setreg
-    -- pattern from above as ghost text at the end of the line, e.g.,
-    --         ...     ?\(\(\_^\|\<|\s\+\)\zs\|\>\)   [>99/>99]
-    -- - SPIKE: Determine what plugin does this.
-    -- Clearing the search highlight doesn't hide the ghost text:
-    --   vim.cmd.nohlsearch()
-    -- Nor does enabling search highlights for the restored "/" register:
-    --   if hlsearch == 1 then
-    --     vim.o.hlsearch = true
-    --   end
-    -- Nor does mimicking LazyVim <Esc> binding ("Escape and Clear hlsearch"):
-    --   vim.cmd("noh")
-    --   LazyVim.cmp.actions.snippet_stop()
-    -- KLUGE: But for some reason, emitting a space works (and thankfully
-    -- doesn't ellicit noice notification popup).
-    print(" ")
-    vim.cmd("nohlsearch")
   end
-  -- Hide the completion menu, otherwise user might delete word
-  -- after ghost test, which makes it more difficult to see the
-  -- edit, and to keep deleting if they need to.
+  -- Hide the completion menu, otherwise user might delete word after
+  -- ghost text (extmark), which makes it more difficult to see the edit,
+  -- and to keep deleting if they need to.
   pcall(function()
     require("blink-cmp").hide()
   end)
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+function M.move_insert_cursor_to_end_of_line()
+  -- DUNNO: Unsure why, but need to both `l` and to call nvim_win_set_cursor...
+  vim.cmd([[normal l]])
+  local cur_win = 0
+  vim.api.nvim_win_set_cursor(cur_win, { vim.fn.line("."), vim.fn.col(".") + 1 })
 end
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
