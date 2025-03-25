d9e238fd6e0c8172825309d3d5fd25f59d659be0
Landon Bouma <landonb@users.noreply.github.com>
2025-03-25 00:12:14 -0500

    refactor: relocate lazy 'build' function to util module

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -523,14 +523,7 @@ return {
     -- - SAVVY: This plugin always appears to have updates in :Lazy.
     -- - SAVVY: Note that lazy does not change directories, e.g.,
     --   vim.fn.getcwd() is wherever user was when they ran Lazy.
-    build = function()
-      vim.fn.system([[
-        cd -- "]] .. vim.fn.stdpath("data") .. "/lazy/snacks.nvim" .. [[" &&
-        git checkout liminal &&
-        git fetch origin &&
-        git rebase origin/main
-        ]])
-    end,
+    build = require("util").lazy_build_fork("snacks.nvim"),
 
     -- -----------------------------------------------------------------
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- lua/util/init.lua
+++ lua/util/init.lua
@@ -12,6 +12,21 @@ local M = {}
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
+-- FTREQ: Pass in specs instead, and get path that specs object.
+
+M.lazy_build_fork = function(proj_dir)
+  return function()
+    vim.fn.system([[
+      cd -- "]] .. vim.fn.stdpath("data") .. "/lazy/" .. proj_dir .. [[" &&
+      git checkout liminal &&
+      git fetch origin &&
+      git rebase origin/main
+      ]])
+  end
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
 function M.augroup(name)
   return vim.api.nvim_create_augroup("lazyb_" .. name, { clear = true })
 end
