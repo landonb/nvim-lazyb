e2fdaaf59362818542e00379519dcd38c009b9e7
Landon Bouma <landonb@users.noreply.github.com>
2025-11-19 14:10:50 -0600

    feat: add Amp Code plugin

---
+++ lua/plugins/amp.lua
@@ -0,0 +1,18 @@
+-- vim:tw=0:ts=2:sw=2:et:ai:ft=lua
+-- Author: Landon Bouma <https://tallybark.com/>
+-- Project: https://github.com/landonb/nvim-lazyb#🧸
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- REFER:
+-- https://github.com/sourcegraph/amp.nvim
+-- https://ampcode.com/manual
+
+return {
+  {
+    "sourcegraph/amp.nvim",
+    branch = "main",
+    lazy = false,
+    opts = { auto_start = true, log_level = "info" },
+  },
+}
